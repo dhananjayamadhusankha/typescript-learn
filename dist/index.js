f9b8e38cc6b344116fdb513e6a84a2217057efc6
dhananjayamadhusankha <mdhananjaya04@gmail.com>
2024-11-14 21:47:42 +0530

    Intersection

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,35 @@
 "use strict";
-let user = [1, 'Dhana'];
+let user = [1, "Dhana"];
 user.push(1);
 console.log(user);
 const mySize = 1;
 console.log(mySize);
+function incomeTax(income, taxYear = 2022) {
+    if (taxYear > 2023) {
+        return income * 1.3;
+    }
+    return 0;
+}
+incomeTax(10000, 2022);
+let employee = {
+    id: 1,
+    name: "Dhana",
+    retire: (date) => {
+        console.log(date);
+    },
+};
+let employee1 = {
+    id: 1,
+    name: "Dhana",
+    retire: (date) => {
+        console.log(date);
+    },
+};
+const kgToLbs = (weight) => {
+    if (typeof weight === "number")
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+};
+kgToLbs(10);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,79 @@
 // array
-let user: [number, string] = [1, 'Dhana']
+let user: [number, string] = [1, "Dhana"];
 
-user.push(1)
+user.push(1);
 
-console.log(user)
+console.log(user);
 
 // enum
 
-const enum Size {Small, Medium, Large}
-const mySize: Size = Size.Medium
+const enum Size {
+  Small,
+  Medium,
+  Large,
+}
+const mySize: Size = Size.Medium;
 
-console.log(mySize)
+console.log(mySize);
 
 //function
-function incomeTax(income:number):number {
-    let x;
-    if (income < 50_000) 
-        return income*1.2
-    return income*1.3
+function incomeTax(income: number, taxYear = 2022): number {
+  if (taxYear > 2023) {
+    return income * 1.3;
+  }
+  return 0;
 }
+
+incomeTax(10_000, 2022);
+
+// objects
+let employee: {
+  id: number;
+  name: string;
+  retire: (date: Date) => void;
+} = {
+  id: 1,
+  name: "Dhana",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+
+// aliase
+type Employee = {
+  id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+
+let employee1: Employee = {
+  id: 1,
+  name: "Dhana",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+
+// union
+const kgToLbs = (weight: number | string): number => {
+  if (typeof weight === "number") return weight * 2.2;
+  else return parseInt(weight) * 2.2;
+};
+
+kgToLbs(10);
+
+// Intersection
+type Dragable = {
+  drag: () => void;
+};
+
+type Resizable = {
+  resize: () => void;
+};
+
+type UIWidget = Dragable & Resizable;
+
+let textBox: UIWidget = {
+  drag: () => {},
+  resize: () => {},
+};
